1e0ecb2a0445b3246188a5606ff7d87a40b3995f
Quinten Muyllaert <quinten.muyllaert@student.howest.be>
2022-12-21 13:49:42 +0100

    ✨ Mobile ht

---
+++ apps/frontend-ocr-test/src/package.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	interface Package {
+		name: string;
+		address: string;
+		postalCode: string;
+		city: string;
+		orderedOn: Date;
+		weight: number;
+		height: number;
+		long: number;
+		lat: number;
+		status: string;
+	}
+
+	import { onMount } from "svelte";
+	let pkg: Package = {
+		name: "John Doe",
+		address: "123 Main St",
+		postalCode: "12345",
+		city: "New York",
+		orderedOn: new Date(),
+		weight: 10,
+		height: 10,
+		long: 10,
+		lat: 10,
+		status: "Delivered",
+	};
+	let name, address, postalCode, city, orderedOn, weight, height, long, lat, status;
+	$: if (pkg) {
+		name = pkg.name;
+		address = pkg.address;
+		postalCode = pkg.postalCode;
+		city = pkg.city;
+		orderedOn = pkg.orderedOn;
+		weight = pkg.weight;
+		height = pkg.height;
+		long = pkg.long;
+		lat = pkg.lat;
+		status = pkg.status;
+	}
+
+	onMount(async () => {
+		console.log(pkg);
+	});
+</script>
+
+<main>
+	<ul>
+		<li>NAME : {name}</li>
+		<li>ADDRESS : {address}</li>
+		<li>POSTAL CODE : {postalCode}</li>
+		<li>CITY : {city}</li>
+		<li>ORDERED ON : {orderedOn.toLocaleDateString()}</li>
+		<li>WEIGHT : {weight}g</li>
+		<li>HEIGHT : {height}cm</li>
+	</ul>
+</main>
+
+<style scoped>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		background-color: rgba(0, 0, 0, 0.2);
+		padding: 1rem;
+		border-radius: 0.25rem;
+		box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
+	}
+	ul {
+		padding: 0;
+		text-align: left;
+		list-style: none;
+	}
+</style>
