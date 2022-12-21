189f5d54c7ba4dd015b4bb79f4d06f53fea91de9
Carmino <carmino.deschuijmree@student.howest.be>
2022-12-21 15:58:43 +0100

    feat: added responsiveness home

--- apps/frontend-customer/src/routes/home.svelte
+++ apps/frontend-customer/src/routes/home.svelte
@@ -2,12 +2,12 @@
 	import { navigate } from "svelte-routing";
 </script>
 
-<div class="shadow rounded-md p-8 bg-white">
+<div class="shadow rounded-md p-8 bg-white w-[80vw] md:w-[30vw]">
 	<div class="flex flex-col gap-4 items-center">
 		<h1 class="font-bold text-2xl">Welkom</h1>
-		<div class="flex gap-4">
-			<button class="bg-red-600 text-white rounded p-4 w-64" on:click={() => navigate("add")}>Toevoegen pakje</button>
-			<button class="bg-red-600 text-white rounded p-4 w-64" on:click={() => navigate("overview")}>Overzicht</button>
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+			<button class="bg-red-600 text-white rounded p-4" on:click={() => navigate("add")}>Toevoegen pakje</button>
+			<button class="bg-red-600 text-white rounded p-4" on:click={() => navigate("overview")}>Overzicht</button>
 		</div>
 	</div>
 </div>
