f133ae21618b4d34ea4a1078964ae05985a702ad
Carmino <carmino.deschuijmree@student.howest.be>
2022-12-21 15:20:19 +0100

    feat: added form

--- apps/frontend-customer/src/routes/add.svelte
+++ apps/frontend-customer/src/routes/add.svelte
@@ -1,47 +1,60 @@
+<script lang="ts">
+	const pkg = {
+		name: "",
+		address: "",
+		postalCode: "",
+		city: "",
+		height: 0,
+		weight: 0,
+		depth: 0,
+		width: 0,
+		long: 50,
+		lat: 3,
+		orderedOn: new Date(),
+		status: "validatie",
+	};
+
+	const handleSubmit = () => {
+		console.log(pkg);
+	};
+</script>
+
 <div class="shadow rounded-md p-8 bg-white">
-	<form class="grid">
-		<h1 class="text-2xl font-bold">Toevoegen</h1>
+	<form class="grid gap-4" on:submit|preventDefault={handleSubmit}>
+		<h1 class="text-2xl font-bold">Pak aangeven</h1>
+
+		<div class="flex flex-col">
+			<label for="naam" class="font-bold">Naam</label>
+			<input type="text" id="naam" class="border py-1 px-2 rounded" bind:value={pkg.name} />
+		</div>
+
+		<div class="flex flex-col">
+			<label for="street" class="font-bold">Straat + nr</label>
+			<input type="text" id="street" class="border py-1 px-2 rounded" bind:value={pkg.address} />
+		</div>
 
 		<div class="flex gap-4">
-			<div class="flex-1">
-				<label for="name">Naam</label>
-				<input type="text" id="name" class="border" />
+			<div class="flex flex-col">
+				<label for="postal" class="font-bold">Postcode</label>
+				<input type="text" id="postal" class="border py-1 px-2 rounded" bind:value={pkg.postalCode} />
+			</div>
+			<div class="flex flex-col">
+				<label for="city" class="font-bold">Stad</label>
+				<input type="text" id="city" class="border py-1 px-2 rounded" bind:value={pkg.city} />
 			</div>
 		</div>
 
 		<div class="flex gap-4">
-			<div class="flex-1">
-				<label for="street">Straat</label>
-				<input type="text" id="street" class="border" />
+			<div class="flex flex-col">
+				<label for="height" class="font-bold">hoogte</label>
+				<input type="number" id="height" class="border py-1 px-2 rounded" bind:value={pkg.height} />
 			</div>
-			<div class="flex-1">
-				<label for="number">Nummer</label>
-				<input type="number" id="number" class="border" />
-			</div>
-		</div>
-
-		<div class="flex gap-4">
-			<div class="flex-1">
-				<label for="postal">Postcode</label>
-				<input type="text" id="postal" class="border" />
-			</div>
-			<div class="flex-1">
-				<label for="city">Stad</label>
-				<input type="text" id="city" />
+			<div class="flex flex-col">
+				<label for="weight" class="font-bold">gewicht</label>
+				<input type="number" id="weight" class="border py-1 px-2 rounded" bind:value={pkg.weight} />
 			</div>
 		</div>
 
-		<div class="flex gap-4">
-			<div class="flex-1">
-				<label for="country">height</label>
-				<input type="number" id="country" class="border" />
-			</div>
-			<div class="flex-1">
-				<label for="phone">weight</label>
-				<input type="number" id="phone" class="border" />
-			</div>
-		</div>
-
-		<button class="bg-red-600 text-white rounded p-2">Toevoegen</button>
+		<button class="bg-red-600 text-white rounded p-2 mt-6">Toevoegen</button>
 	</form>
 </div>
