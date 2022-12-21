2bfb2d2c75cd099777efe49b997f30f9bc46ab53
BostoenToby <toby.bostoen@student.howest.be>
2022-12-21 14:14:07 +0100

    feat: data in frontend driver

--- apps/frontend-driver/src/routes/home.svelte
+++ apps/frontend-driver/src/routes/home.svelte
@@ -1,4 +1,33 @@
+<script lang="ts">
+	import { navigate } from "svelte-routing"
+	import { onMount } from "svelte";
+	import { writable } from "svelte/store";
+
+	const packages = writable<any>(null);
+
+	const getPackages = async () => {
+		const res = await fetch("http://backend-driver.localhost/packages");
+		return await res.json();
+	};
+
+	onMount(async () => {
+		await getPackages().then((data) => {
+			console.log(data);
+			packages.set(data)
+		});
+	});
+</script>
+
 <div class="shadow rounded-md p-8 bg-white">
 	<h1>Home</h1>
 	<p>Welkom op de homepagina</p>
+	{#if $packages != null}
+		{#each $packages as pkg}
+			<button on:click={() => {
+				navigate(`/tracking/${pkg.id}`)
+			}} class="px-4 py-2 bg-red-600 text-white">
+				{pkg.id}
+			</button>
+		{/each}
+	{/if}
 </div>
