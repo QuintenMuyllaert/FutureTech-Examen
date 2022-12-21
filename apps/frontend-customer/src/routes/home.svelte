9b0a0f60f297d046d4aa800fb39c2c0501387cc5
Carmino <carmino.deschuijmree@student.howest.be>
2022-12-21 15:01:11 +0100

    feat: added home & add

---
+++ apps/frontend-customer/src/routes/home.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { navigate } from "svelte-routing";
+</script>
+
+<div class="shadow rounded-md p-8 bg-white">
+	<div class="flex flex-col gap-4 items-center">
+		<h1 class="font-bold text-2xl">Welkom</h1>
+		<div class="flex gap-4">
+			<button class="bg-red-600 text-white rounded p-4 w-64" on:click={() => navigate("add")}>Toevoegen pakje</button>
+			<button class="bg-red-600 text-white rounded p-4 w-64" on:click={() => navigate("tracking")}>Tracking</button>
+		</div>
+	</div>
+</div>
